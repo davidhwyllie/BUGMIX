7a16ac8de28b4e8f28bab47cca19fb677e100402
David Wyllie <david.wyllie@ndm.ox.ac.uk>
2017-02-15 14:36:31 +0000

    Parsing to database implemented

--- src/findFilesToProcess3.py
+++ src/findFilesToProcess3.py
@@ -21,11 +21,17 @@ import pandas as pd
 # use a list of samples which are provided in a flat file.
 # they are TB samples from either B'ham or Brighton.
 
-# read these from the flat file, and divide the guids into 15 approx equal sized files.
-inputfile="/mnt/microbio/HOMES/dwyllie/data/TBMIX2017/input/QCSummaryPHETB_2017-01-29_00-20-41.txt"
-df=pd.read_table(inputfile)
-print("Read data frame.")
-print(df['Sample ID Guuid'])
+# read these from the flat file, and divide the guids into approx equal sized files.
+to_process=[]
+globpattern="/mnt/microbio/ndm-hicf/ogre/pipeline_output/*/MAPPING/2e6b7bc7-f52c-4649-8538-c984ab3894bb_R00000039/STD/basecalls/*v3.vcf.gz"
+for inputfile in glob.glob(globpattern):
+    guid=os.path.basename(inputfile)[0:36]
+    # test whether the file has already been parsed
+    if os.path.exists(os.path.join('/home/local/GEL/dwyllie/data/TBMIX2017/db','{0}.db'.format(guid))):
+        print('exists {0}'.format(guid))
+    else:
+        print('queuing for processing: {0}'.format(guid))
+        to_process.append(guid)
 
 # divide the guids into 10 output files 
 inputdir='/home/local/GEL/dwyllie/data/TBMIX2017'
@@ -34,9 +40,9 @@ outputfiles=dict()
 for i in range(10):
     outputfiles[i]=open(os.path.join(inputdir,str(i)+'.toprocess'),'wb')
 
-for (i, guid) in enumerate(df['Sample ID Guuid']):
-    print(i, i % 15, guid)
-    outputfiles[i % 15 ].write("{0}\n".format(guid))
+for (i, guid) in enumerate(to_process):
+    print(i, i % 10, guid)
+    outputfiles[i % 10 ].write("{0}\n".format(guid))
     
 for i in outputfiles.keys():
     outputfiles[i].close()
