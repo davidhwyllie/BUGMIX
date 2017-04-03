8c1a06967a8a3284b6a963ca04437cf81feb70d9
David Wyllie <david.wyllie@ndm.ox.ac.uk>
2017-04-03 10:12:44 +0100

    Working python3 version

--- src/extractResults.py
+++ src/extractResults.py
@@ -23,30 +23,36 @@ from parseVcf import multiMixtureReader, summaryStore, db1
 # they are TB samples from either B'ham or Brighton.
 
 # load reference data
+
+# read the data about the files to be analysed
+nGuids=0
+sampleIds=list()
+for filename in glob.glob("/home/dwyllie/data/TBMIX/db/*.db"):
+	guid=os.path.basename(filename)[0:36]
+	sampleIds.append(guid)
+	nGuids+=1
+print("Reading guids, n= {0}".format(nGuids))
+
+
 print("Loading deep branch information")
-tbd_file="/mnt/microbio/HOMES/dwyllie/dev/TBMIX/refdata/TBDeepBranch.txt"
+tbd_file="/home/dwyllie/dev/TBMIX/refdata/TBDeepBranch.txt"
 coll=pd.read_table(tbd_file)
 collsites=coll['position']
 
-# read the data about the files to be analysed
-print("Loading information on TB samples")
-inputfile="/mnt/microbio/HOMES/dwyllie/data/TBMIX2017/input/QCSummaryPHETB_2017-01-29_00-20-41.txt"
-df=pd.read_table(inputfile)
-sampleIds=df['Sample ID Guuid']
-
 # create a summaryStore object;
-basedir='/home/local/GEL/dwyllie/data/TBMIX2017/'
+basedir='/home/dwyllie/data/TBMIX'
 targetDir=os.path.join(basedir,'db')
-outputDir="/mnt/microbio/HOMES/dwyllie/data/TBMIX2017/output"
-persistenceDir=outputDir
+outputDir="/home/dwyllie/data/TBMIX/output"
 dbname='sstat'
 test_path="<<DEFAULT>>/%s.db" % dbname          # used for testing LsStore object
 test_connstring="sqlite:///%s" % test_path
-sstat=summaryStore(db=db1, engineName=test_connstring, persistenceDir=outputDir)
+print(test_connstring)
+
+sstat=summaryStore(db=db1, engineName=test_connstring, dbDir=targetDir)
 
 # compute summaries of all the files using a filter and for a selection;
-sampleIds2=sampleIds[1:500]
-mmr1=multiMixtureReader(sampleIds=sampleIds2, persistenceDir=targetDir, this_summaryStore=None)     #, this_summaryStore=sstat
+sampleIds2=sampleIds[1:50]
+mmr1=multiMixtureReader(sampleIds=sampleIds2, persistenceDir=targetDir, this_summaryStore=sstat)     #, this_summaryStore=sstat
 mmr1.summarise_byFilter(minDepth=50, minP=8)
 print(mmr1.df)
 print("Summary complete")
@@ -55,10 +61,7 @@ mmr1.summarise_selection(collsites)
 print("Summary complete")
 print(mmr1.df)
 exit()
-print("There are {0} sampleIds.".format(len(sampleIds)))
-sampleIds1=sampleIds2
-mmr=multiMixtureReader(sampleIds=sampleIds1, persistenceDir=targetDir)
-exit()
+
 
 mmr.read_selection(collsites)
 print(mmr.df)
